c3255e0bb9ee0a08ed46820607e9327ff93182f5
demohcmus <96030423+demohcmus@users.noreply.github.com>
2022-01-04 21:56:40 +0700

    update website

--- js/custom.js
+++ js/custom.js
@@ -45,3 +45,83 @@ $('#portfolio a').nivoLightbox({
    $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
+
+// phần chuyền hình ảnh 
+    var responsiveSlider = function() {
+
+      var slider = document.getElementById("slider");
+      var sliderWidth = slider.offsetWidth;
+      var slideList = document.getElementById("slideWrap");
+      var count = 1;
+      var items = slideList.querySelectorAll("li").length;
+      var prev = document.getElementById("prev");
+      var next = document.getElementById("next");
+      
+      window.addEventListener('resize', function() {
+        sliderWidth = slider.offsetWidth;
+      });
+      
+      var prevSlide = function() {
+        if(count > 1) {
+          count = count - 2;
+          slideList.style.left = "-" + count * sliderWidth + "px";
+          count++;
+        }
+        else if(count = 1) {
+          count = items - 1;
+          slideList.style.left = "-" + count * sliderWidth + "px";
+          count++;
+        }
+      };
+      
+      var nextSlide = function() {
+        if(count < items) {
+          slideList.style.left = "-" + count * sliderWidth + "px";
+          count++;
+        }
+        else if(count = items) {
+          slideList.style.left = "0px";
+          count = 1;
+        }
+      };
+      
+      next.addEventListener("click", function() {
+        nextSlide();
+      });
+      
+      prev.addEventListener("click", function() {
+        prevSlide();
+      });
+      
+      setInterval(function() {
+        nextSlide()
+      }, 5000);
+      
+      };
+      
+      window.onload = function() {
+      responsiveSlider();  
+      }
+      
+
+
+      // phần button
+      //Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
